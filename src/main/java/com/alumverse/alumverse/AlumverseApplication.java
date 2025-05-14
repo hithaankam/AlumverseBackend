9d3147148d3f5f4259caf605a59648fc6df02a37
hithaankam <23wh1a1289@bvrithyderabad.edu.in>
2025-05-14 19:37:28 +0530

    created base spring boot application

---
+++ src/main/java/com/alumverse/alumverse/AlumverseApplication.java
@@ -0,0 +1,13 @@
+package com.alumverse.alumverse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlumverseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AlumverseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/alumverse/alumverse/AlumverseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alumverse.alumverse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlumverseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
