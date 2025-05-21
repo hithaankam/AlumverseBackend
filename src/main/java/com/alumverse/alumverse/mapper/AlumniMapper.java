df046890cebc9d02ed8372f8973982c30ad7b988
hithaankam <23wh1a1289@bvrithyderabad.edu.in>
2025-05-21 14:24:12 +0530

    Created mapper class to handle mapping between Alumni and AlumniDto objects

--- src/main/java/com/alumverse/alumverse/mapper/AlumniMapper.java
+++ src/main/java/com/alumverse/alumverse/mapper/AlumniMapper.java
@@ -1,4 +1,26 @@
 package com.alumverse.alumverse.mapper;
 
+import com.alumverse.alumverse.dto.AlumniDto;
+import com.alumverse.alumverse.entity.Alumni;
+
 public class AlumniMapper {
+    public static AlumniDto mapToAlumniDto(Alumni alumni){
+        return new AlumniDto(
+                alumni.getId(),
+          alumni.getFirstName(),
+          alumni.getLastName(),
+          alumni.getEmail()
+
+
+        );
+    }
+
+    public static Alumni mapToAlumni(AlumniDto alumniDto){
+        return new Alumni(
+                alumniDto.getId(),
+                alumniDto.getFirstName(),
+                alumniDto.getLastName(),
+                alumniDto.getEmail()
+        );
+    }
 }
