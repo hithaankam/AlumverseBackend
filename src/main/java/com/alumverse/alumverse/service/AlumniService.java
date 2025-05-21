67544884c1c7602da4070addd3652bbadeb84672
hithaankam <23wh1a1289@bvrithyderabad.edu.in>
2025-05-21 14:27:07 +0530

    Created service layer to provide data handling methods for controller layer

--- src/main/java/com/alumverse/alumverse/service/AlumniService.java
+++ src/main/java/com/alumverse/alumverse/service/AlumniService.java
@@ -1,4 +1,13 @@
 package com.alumverse.alumverse.service;
 
+import com.alumverse.alumverse.dto.AlumniDto;
+
+import java.util.List;
+
 public interface AlumniService {
+    AlumniDto createAlumni(AlumniDto alumnidto);
+    AlumniDto getAlumniById(Long alumniId);
+    List<AlumniDto> getAllAlumni();
+    AlumniDto updateAlumni(Long alumniId, AlumniDto updatedAlumni);
+    void deleteAlumni(Long alumniId);
 }
--- src/main/java/com/alumverse/alumverse/service/impl/AlumniServiceImpl.java
+++ src/main/java/com/alumverse/alumverse/service/impl/AlumniServiceImpl.java
@@ -1,4 +1,62 @@
 package com.alumverse.alumverse.service.impl;
 
-public class AlumniServiceImpl {
+import com.alumverse.alumverse.dto.AlumniDto;
+import com.alumverse.alumverse.entity.Alumni;
+import com.alumverse.alumverse.exception.ResourceNotFoundException;
+import com.alumverse.alumverse.mapper.AlumniMapper;
+import com.alumverse.alumverse.repository.AlumniRepository;
+import com.alumverse.alumverse.service.AlumniService;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@AllArgsConstructor
+public class AlumniServiceImpl implements AlumniService {
+    private AlumniRepository alumniRepository;
+    @Override
+    public AlumniDto createAlumni(AlumniDto alumnidto) {
+        Alumni alumni = AlumniMapper.mapToAlumni(alumnidto);
+        Alumni savedAlumni = alumniRepository.save(alumni);
+
+        return AlumniMapper.mapToAlumniDto(savedAlumni);
+    }
+
+    @Override
+    public AlumniDto getAlumniById(Long alumniId) {
+       Alumni alumni = alumniRepository.findById(alumniId)
+        .orElseThrow(() ->
+                new ResourceNotFoundException("Alumni Not Found with Id: " + alumniId));
+       return AlumniMapper.mapToAlumniDto(alumni);
+    }
+
+    @Override
+    public List<AlumniDto> getAllAlumni() {
+        List<Alumni> allAlumni= alumniRepository.findAll();
+        return allAlumni.stream().map((alumni) -> AlumniMapper.mapToAlumniDto(alumni))
+                .collect(Collectors.toList());
+    }
+    @Override
+    public AlumniDto updateAlumni(Long alumniId, AlumniDto alumnidto) {
+        Alumni alumni = alumniRepository.findById(alumniId)
+                .orElseThrow(() ->
+                        new ResourceNotFoundException("Alumni Not Found with Id: " + alumniId));
+        alumni.setFirstName(alumnidto.getFirstName());
+        alumni.setLastName(alumnidto.getLastName());
+        alumni.setEmail(alumnidto.getEmail());
+        Alumni updatedAlumni = alumniRepository.save(alumni);
+        return AlumniMapper.mapToAlumniDto(updatedAlumni);
+    }
+
+    @Override
+    public void deleteAlumni(Long alumniId) {
+        Alumni alumni = alumniRepository.findById(alumniId)
+                .orElseThrow(() ->
+                        new ResourceNotFoundException("Alumni Not Found with Id: " + alumniId));
+        alumniRepository.deleteById(alumniId);
+    }
+
+
 }
